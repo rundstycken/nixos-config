0628c89129ee5f5bcafdddd1ecd1b7f8c27ed40d
Rundstycken <Rundstycken@gmail.com>
2024-05-15 14:01:09 +0200

    Downgrade to 23.11

--- system/flake.nix
+++ system/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # NixOS official package source, using the nixos-23.11 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
