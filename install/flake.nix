bbc31008f46f2bfe4bfa8c620a6c575182b84da5
rundstycken <156079760+rundstycken@users.noreply.github.com>
2024-05-17 09:24:16 +0200

    Create flake.nix install file

---
+++ install/flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = inputs@{ self, nixpkgs, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ 
+      ./configuration.nix 
+      inputs.disko.nixosModules.disko
+      ];
+    };
+  };
+}
